08c40178ea797b208d1353138ff5ef66609146a3
anawayjosh <70968396+anaway-69@users.noreply.github.com>
2021-02-16 12:11:58 -0500

    Update 01_hello_script.sh

--- 01_hello_script.sh
+++ 01_hello_script.sh
@@ -2,9 +2,16 @@
 #This is how u specify a comment!!
 echo "Hello bash Scripting"
 
-#This is how u create a hearDoc!
+
+
+
+
+# Our question is what is HereDoc?
+#--> In Bash and other shells like Zsh, a Here document (Heredoc) is a type of redirection that allows you to pass multiple lines of input to a command. ... If the delimiting identifier is unquoted, the shell will substitute all variables, commands and special characters before passing the here-document lines to the command
+
+#This is how u create a hereDoc!
 
 cat << kreative
-this is heardoc delimeter
+this is heredoc delimeter
 and u can write anything
 kreative
